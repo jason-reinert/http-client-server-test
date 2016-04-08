990b1e3adcccef5001fb4403a2e22604b7cae6a0
jason-reinert <jason.reinert@pillrcompany.com>
2016-04-08 16:20:25 -0400

    updated shell index.php script

--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 require "../vendor/autoload.php";
+
 # TIP: Use the $_SERVER Sugerglobal to get all the data your need from the Client's HTTP Request.
+
+# TIP: HTTP headers are printed natively in PHP by invoking header().
+#      Ex. header('Content-Type', 'text/html');
