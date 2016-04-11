759967cd05735bfd7d1b6bbd78441fbdb228c294
Jason Reinert <jason.reinert@pillrcompany.com>
2016-04-11 15:37:25 -0400

    Update TestHttpServer.php

--- tests/TestHttpServer.php
+++ tests/TestHttpServer.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use \pillr\library\http\Request  as HttpRequest;
 use \pillr\library\http\Response as HttpResponse;
